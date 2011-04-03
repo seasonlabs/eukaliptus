6c8e09b403c6b16d8a22fc72d33d455584816cf3
Victor Castell <victorcoder@gmail.com>
2011-04-03 23:41:45 +0200

    Facebook helper

--- lib/eukaliptus/middleware.rb
+++ lib/eukaliptus/middleware.rb
@@ -11,7 +11,7 @@ module Eukaliptus
 
       @response.header["P3P"] = 'CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"'
 
-      cookie_fix(env)# if env['PATH_INFO'] == '/cookie_fix'
+      cookie_fix(env) if env['PATH_INFO'] == '/cookie_fix'
 
       @response.finish
     end
@@ -22,18 +22,13 @@ module Eukaliptus
 
       if params['_session_id']
         session = ActiveSupport::JSON.decode(params['_session_id'])
+
+        unless (@request.cookies['fbs_' + Facebook::APP_ID.to_s].present?)
+          session = session.map { |key, value| key.to_s + "=" + value.to_s }.join("&")
+          @response.set_cookie('fbs_' + Facebook::APP_ID.to_s, session)
+        end
+        @response.redirect(params['redirect_to'], 200)
       end
-
-      unless (@request.cookies['fbs_' + Facebook::APP_ID.to_s].present?)
-        session = session.map { |key, value| key.to_s + "=" + value.to_s }.join("&")
-        cookies['fbs_' + Facebook::APP_ID.to_s] = session
-      end
-      redirect_to params[:redirect_to] and return
-
-      @response.set_cookie("foo", {:value => "bar"})
-
-
-      @response.status = 200
     end
   end
 end
--- lib/eukaliptus/railtie.rb
+++ lib/eukaliptus/railtie.rb
@@ -1,16 +1,25 @@
 require 'eukaliptus/middleware'
 require 'rails'
-  
+require 'view_helpers/facebook_helper'
+
+module Facebook
+end
+
 module Eukaliptus
   class Railtie < Rails::Railtie
     # Load rake tasks
     rake_tasks do
-      load File.join(File.dirname(__FILE__), 'tasks/facebook.rake')
+      load File.join(File.dirname(__FILE__), '../tasks/facebook.rake')
     end
     
     initializer "eukaliptus.initializer", :after => :after_initialize do |app|
       #ActionView::Base.send :include, Helper
       app.middleware.use Eukaliptus::Middleware
+
+      CONFIG = YAML.load_file(Rails.root.join("config/facebook.yml"))[Rails.env]
+      Facebook::APP_ID = CONFIG['app_id']
+      Facebook::SECRET = CONFIG['secret_key']
+      Facebook::CANVAS = CONFIG['canvas']
     end
   end
 end
