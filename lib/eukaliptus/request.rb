77d9bf35566b2142832f01139d892c111583a437
Victor Castell <victorcoder@gmail.com>
2011-06-12 20:01:46 +0200

    Real param parsing stolen from Goliath

--- lib/eukaliptus/request.rb
+++ lib/eukaliptus/request.rb
@@ -2,12 +2,42 @@ require 'rack/request'
 
 # Extend Rack::Request to add some helpers for FB
 class Request < ::Rack::Request
+  URL_ENCODED = %r{^application/x-www-form-urlencoded}
+  JSON_ENCODED = %r{^application/json}
+
   def params
-    rack_input = env["rack.input"].read
-    @params ||= Rack::Utils.parse_query(rack_input, "&")
+    @params ||= retrieve_params(env)
   end
 
   def facebook?
-    params["signed_request"].present?
+    params[:signed_request].present?
   end
+
+  private
+    # Stolen from Goliath
+    def retrieve_params(env)
+      params = {}
+      params.merge!(::Rack::Utils.parse_nested_query(env['QUERY_STRING']))
+
+      if env['rack.input']
+        post_params = ::Rack::Utils::Multipart.parse_multipart(env)
+        unless post_params
+          body = env['rack.input'].read
+          env['rack.input'].rewind
+
+          post_params = case(env['CONTENT_TYPE'])
+          when URL_ENCODED then
+            ::Rack::Utils.parse_nested_query(body)
+          when JSON_ENCODED then
+            MultiJson.decode(body)
+          else
+            {}
+          end
+        end
+
+        params.merge!(post_params)
+      end
+
+      params
+    end
 end
--- lib/eukaliptus/version.rb
+++ lib/eukaliptus/version.rb
@@ -1,3 +1,3 @@
 module Eukaliptus
-  VERSION = "0.7.1"
+  VERSION = "0.8.1"
 end
